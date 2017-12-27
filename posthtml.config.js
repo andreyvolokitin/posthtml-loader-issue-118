2b96ebd8019eebdae10fe3b56ca9388f2f2c77de
andreyvolokitin <andreyvlktn@gmail.com>
2017-12-27 12:50:34 +0400

    initial commit

---
+++ posthtml.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    'posthtml-include': {},
+  },
+  /*plugins: [
+    require('posthtml-include')({}),
+  ],*/
+};
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './index.js',
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+          {
+            loader: 'posthtml-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+    }),
+  ],
+};
